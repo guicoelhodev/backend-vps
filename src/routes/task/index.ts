7e492601eab7cf2f63a1c37f1d18b42b4fd21ba0
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-24 11:13:41 -0300

    feat(Task): create update and post request

--- src/routes/task/index.ts
+++ src/routes/task/index.ts
@@ -1,8 +1,27 @@
 import { FastifyInstance } from "fastify";
+import z from "zod";
 
-type TTask = { id: string; name: string; completed: boolean };
+const createTaskSchema = z.object({
+	description: z.string().min(1, "Name is required"),
+});
 
-const taskList: TTask[] = [];
+const updateTaskSchema = z
+	.object({
+		description: z.string().optional(),
+		completed: z.boolean().optional(),
+		id: z.string().min(1, "Task ID is required"),
+	})
+	.refine(
+		(data) => data.description !== undefined || data.completed !== undefined,
+		{
+			message: "At least one of 'description' or 'completed' must be provided",
+			path: [],
+		},
+	);
+
+type TCreateSchema = { description: string; id: string; completed: boolean };
+
+const taskList: TCreateSchema[] = [];
 
 export async function taskRoutes(server: FastifyInstance) {
 	server.get("/", (_, reply) => {
@@ -12,18 +31,49 @@ export async function taskRoutes(server: FastifyInstance) {
 	});
 
 	server.post("/", (request, reply) => {
-		const body = request.body as { name: string };
+		const parseResult = createTaskSchema.safeParse(request.body);
 
-		if (!body.name) {
-			return reply.status(400).send({ error: "Name is required" });
+		if (!parseResult.success) {
+			return reply.status(400).send({
+				errors: parseResult.error.issues,
+			});
 		}
 
 		const taskId = crypto.randomUUID();
 
-		taskList.push({ id: taskId, name: body.name, completed: false });
+		taskList.push({
+			id: taskId,
+			description: parseResult.data.description,
+			completed: false,
+		});
 
 		reply.status(201).send({
 			message: `Task created: ${taskId}`,
+		});
+	});
+
+	server.put("/", (request, reply) => {
+		const parseResult = updateTaskSchema.safeParse(request.body);
+
+		if (!parseResult.success) {
+			return reply.status(400).send({
+				errors: parseResult.error.issues,
+			});
+		}
+
+		const id = parseResult.data.id;
+		const indexIdList = taskList.findIndex((i) => i.id === id);
+
+		if (indexIdList === -1) {
+			return reply.status(400).send({
+				errors: "task ID not found",
+			});
+		}
+
+		Object.assign(taskList[indexIdList], parseResult.data);
+
+		return reply.status(200).send({
+			message: `Update task ${id} successfully`,
 		});
 	});
 }
