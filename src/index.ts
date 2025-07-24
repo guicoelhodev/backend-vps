13635e2462d8d426d13e9e0960ee0e2ecf2fcae9
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-24 09:48:41 -0300

    refactor: fix initial tsconfig file

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import fastify from "fastify";
 
 const server = fastify();
 
-server.get("/ping", async (request, reply) => {
-	return "ping\n";
+server.get("/", (_, reply) => {
+	reply.send({ hello: "world" });
 });
 
 server.listen({ port: 8080 }, (err, address) => {
 	if (err) {
-		console.error(err);
+		server.log.error(err);
 		process.exit(1);
 	}
-	console.log(`Server listening at ${address}`);
+
+	console.log(`Server is now listening on ${address}`);
 });
