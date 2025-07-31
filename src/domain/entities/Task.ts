182b40152fedcf3303066c653f3a7624069dc0ae
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-31 10:55:25 -0300

    feat(domain): create Task entitie

---
+++ src/domain/entities/Task.ts
@@ -0,0 +1,14 @@
+type TTask = {
+	id: string;
+	description: string;
+	completed: boolean;
+	createdAt: Date;
+};
+
+export class Task {
+	constructor(private task: TTask) { }
+
+	getTask() {
+		return this.task;
+	}
+}
