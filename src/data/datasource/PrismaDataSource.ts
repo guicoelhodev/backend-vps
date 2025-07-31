d6c3c16e37909dc9766bcdec9006f5fb2786f74c
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-31 11:58:28 -0300

    feat(data): create Prisma connection singleton class

--- src/data/datasource/PrismaDataSource.ts
+++ src/data/datasource/PrismaDataSource.ts
@@ -1,23 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
 export class PrismaDataSource {
-	static instance: PrismaClient;
+  static instance: PrismaClient;
 
-	static getInstance() {
-		if (!this.instance) {
-			PrismaDataSource.instance = new PrismaClient();
-		}
+  static getInstance(): PrismaClient {
+    if (!this.instance) {
+      PrismaDataSource.instance = new PrismaClient();
+    }
 
-		return this.instance;
-	}
+    return this.instance;
+  }
 
-	static async connect() {
-		try {
-			await PrismaDataSource.getInstance().$connect();
+  static async connect() {
+    try {
+      await PrismaDataSource.getInstance().$connect();
 
-			console.log("Database connected succesfully");
-		} catch (error) {
-			throw new Error(`Data base connection failed, ${error}`);
-		}
-	}
+      console.log("Database connected succesfully");
+    } catch (error) {
+      throw new Error(`Data base connection failed, ${error}`);
+    }
+  }
 }
